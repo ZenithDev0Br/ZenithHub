14444581567001f84487d334631454cc15a03f51
ZenithDev0Br <zenithdev.br@gmail.com>
2026-05-26 19:11:58 -0300

    Update FarmSettings.lua

--- Modules/FarmSettings.lua
+++ Modules/FarmSettings.lua
@@ -1,14 +1,13 @@
+if not getgenv().ZenithHub then getgenv().ZenithHub = { Modules = {} } end
 local FarmSettings = {}
 local LP = game:GetService("Players").LocalPlayer
 
--- Padrões já ativados
 _G.FastAttack = true
 _G.BringMobs = true
 _G.TweenSpeed = 300
 _G.AttackHeight = 15
-_G.AttackDistance = 0 -- 0 significa exatamente em cima. Valores maiores afastam o player.
+_G.AttackDistance = 0
 
--- Função auxiliar para checar se algum Farm está rodando
 function FarmSettings:IsFarming()
     return _G.AutoFarmLevel or _G.AutoFarmBones or _G.AutoAnnihilateBosses
 end
@@ -16,8 +15,7 @@ end
 function FarmSettings:FastAttack(enabled)
     _G.FastAttack = enabled
     task.spawn(function()
-        while task.wait(0.1) do
-            -- SÓ ATACA SE O FAST ATTACK TIVER ON *E* ALGUM FARM TIVER RODANDO
+        while task.wait(0.25) do -- Atualizado para 0.25 para evitar o kick de DMGDEBUG
             if _G.FastAttack and self:IsFarming() then
                 pcall(function()
                     local Combat = LP.Character:FindFirstChildOfClass("Tool")
@@ -34,11 +32,9 @@ function FarmSettings:BringMobs(enabled)
     _G.BringMobs = enabled
     task.spawn(function()
         while task.wait(0.5) do
-            -- SÓ PUXA SE O BRING MOBS TIVER ON *E* ALGUM FARM TIVER RODANDO
             if _G.BringMobs and self:IsFarming() then
                 for _, v in pairs(workspace.Enemies:GetChildren()) do
-                    if v:FindFirstChild("HumanoidRootPart") and LP.Character:FindFirstChild("HumanoidRootPart") then
-                        -- Puxa o mob para a frente do jogador
+                    if v:FindFirstChild("HumanoidRootPart") and v.Name == _G.CurrentMobName then
                         v.HumanoidRootPart.CFrame = LP.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0, -5)
                     end
                 end
