618494c2d70b20674348dd266183acadb6ac9ba4
ZenithDev0Br <zenithdev.br@gmail.com>
2026-05-26 01:21:27 -0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,20 +1,4 @@
 local ZenithHub = getgenv().ZenithHub
-local Core = ZenithHub.Core
+local Info = ZenithHub.Modules.InfoService
 
-local Settings = ZenithHub.Settings
-
-Core:Notify("Main Loaded")
-
-task.spawn(function()
-    while task.wait(1) do
-
-        if Settings.Enabled then
-            local root = Core:GetRoot()
-            if root then
-                -- lógica principal do sistema
-                print("Running main loop")
-            end
-        end
-
-    end
-end)
+Info:Start()
