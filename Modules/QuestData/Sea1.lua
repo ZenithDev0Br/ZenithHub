d56920255dc5fd79a060318944ed6dcd61d949a3
ZenithDev0Br <zenithdev.br@gmail.com>
2026-05-27 00:43:11 -0300

    Create Sea1.lua

---
+++ Modules/QuestData/Sea1.lua
@@ -0,0 +1,420 @@
+return {
+
+    {
+        Min = 1,
+        Max = 15,
+
+        QuestName = "BanditQuest1",
+        QuestLevel = 1,
+
+        Enemy = "Bandit",
+
+        QuestPosition = Vector3.new(
+            1060,
+            17,
+            1547
+        )
+    },
+
+    {
+        Min = 15,
+        Max = 30,
+
+        QuestName = "JungleQuest",
+        QuestLevel = 1,
+
+        Enemy = "Monkey",
+
+        QuestPosition = Vector3.new(
+            -1604,
+            37,
+            152
+        )
+    },
+
+    {
+        Min = 30,
+        Max = 40,
+
+        QuestName = "JungleQuest",
+        QuestLevel = 2,
+
+        Enemy = "Gorilla",
+
+        QuestPosition = Vector3.new(
+            -1604,
+            37,
+            152
+        )
+    },
+
+    {
+        Min = 40,
+        Max = 55,
+
+        QuestName = "BuggyQuest1",
+        QuestLevel = 1,
+
+        Enemy = "Pirate",
+
+        QuestPosition = Vector3.new(
+            -1140,
+            5,
+            3828
+        )
+    },
+
+    {
+        Min = 55,
+        Max = 60,
+
+        QuestName = "BuggyQuest1",
+        QuestLevel = 2,
+
+        Enemy = "Brute",
+
+        QuestPosition = Vector3.new(
+            -1140,
+            5,
+            3828
+        )
+    },
+
+    {
+        Min = 60,
+        Max = 75,
+
+        QuestName = "DesertQuest",
+        QuestLevel = 1,
+
+        Enemy = "Desert Bandit",
+
+        QuestPosition = Vector3.new(
+            897,
+            7,
+            4389
+        )
+    },
+
+    {
+        Min = 75,
+        Max = 90,
+
+        QuestName = "DesertQuest",
+        QuestLevel = 2,
+
+        Enemy = "Desert Officer",
+
+        QuestPosition = Vector3.new(
+            897,
+            7,
+            4389
+        )
+    },
+
+    {
+        Min = 90,
+        Max = 100,
+
+        QuestName = "SnowQuest",
+        QuestLevel = 1,
+
+        Enemy = "Snow Bandit",
+
+        QuestPosition = Vector3.new(
+            1385,
+            87,
+            -1302
+        )
+    },
+
+    {
+        Min = 100,
+        Max = 120,
+
+        QuestName = "SnowQuest",
+        QuestLevel = 2,
+
+        Enemy = "Snowman",
+
+        QuestPosition = Vector3.new(
+            1385,
+            87,
+            -1302
+        )
+    },
+
+    {
+        Min = 120,
+        Max = 150,
+
+        QuestName = "MarineQuest2",
+        QuestLevel = 1,
+
+        Enemy = "Chief Petty Officer",
+
+        QuestPosition = Vector3.new(
+            -5036,
+            29,
+            4325
+        )
+    },
+
+    {
+        Min = 150,
+        Max = 175,
+
+        QuestName = "SkyQuest",
+        QuestLevel = 1,
+
+        Enemy = "Sky Bandit",
+
+        QuestPosition = Vector3.new(
+            -4839,
+            718,
+            -2620
+        )
+    },
+
+    {
+        Min = 175,
+        Max = 190,
+
+        QuestName = "SkyQuest",
+        QuestLevel = 2,
+
+        Enemy = "Dark Master",
+
+        QuestPosition = Vector3.new(
+            -4839,
+            718,
+            -2620
+        )
+    },
+
+    {
+        Min = 190,
+        Max = 210,
+
+        QuestName = "PrisonerQuest",
+        QuestLevel = 1,
+
+        Enemy = "Prisoner",
+
+        QuestPosition = Vector3.new(
+            5310,
+            2,
+            474
+        )
+    },
+
+    {
+        Min = 210,
+        Max = 250,
+
+        QuestName = "PrisonerQuest",
+        QuestLevel = 2,
+
+        Enemy = "Dangerous Prisoner",
+
+        QuestPosition = Vector3.new(
+            5310,
+            2,
+            474
+        )
+    },
+
+    {
+        Min = 250,
+        Max = 275,
+
+        QuestName = "ColosseumQuest",
+        QuestLevel = 1,
+
+        Enemy = "Toga Warrior",
+
+        QuestPosition = Vector3.new(
+            -1580,
+            8,
+            -2982
+        )
+    },
+
+    {
+        Min = 275,
+        Max = 300,
+
+        QuestName = "ColosseumQuest",
+        QuestLevel = 2,
+
+        Enemy = "Gladiator",
+
+        QuestPosition = Vector3.new(
+            -1580,
+            8,
+            -2982
+        )
+    },
+
+    {
+        Min = 300,
+        Max = 330,
+
+        QuestName = "MagmaQuest",
+        QuestLevel = 1,
+
+        Enemy = "Military Soldier",
+
+        QuestPosition = Vector3.new(
+            -5316,
+            12,
+            8515
+        )
+    },
+
+    {
+        Min = 330,
+        Max = 375,
+
+        QuestName = "MagmaQuest",
+        QuestLevel = 2,
+
+        Enemy = "Military Spy",
+
+        QuestPosition = Vector3.new(
+            -5316,
+            12,
+            8515
+        )
+    },
+
+    {
+        Min = 375,
+        Max = 400,
+
+        QuestName = "FishmanQuest",
+        QuestLevel = 1,
+
+        Enemy = "Fishman Warrior",
+
+        QuestPosition = Vector3.new(
+            61122,
+            18,
+            1569
+        )
+    },
+
+    {
+        Min = 400,
+        Max = 450,
+
+        QuestName = "FishmanQuest",
+        QuestLevel = 2,
+
+        Enemy = "Fishman Commando",
+
+        QuestPosition = Vector3.new(
+            61122,
+            18,
+            1569
+        )
+    },
+
+    {
+        Min = 450,
+        Max = 475,
+
+        QuestName = "SkyExp1Quest",
+        QuestLevel = 1,
+
+        Enemy = "God's Guard",
+
+        QuestPosition = Vector3.new(
+            -7859,
+            5545,
+            -381
+        )
+    },
+
+    {
+        Min = 475,
+        Max = 525,
+
+        QuestName = "SkyExp1Quest",
+        QuestLevel = 2,
+
+        Enemy = "Shanda",
+
+        QuestPosition = Vector3.new(
+            -7859,
+            5545,
+            -381
+        )
+    },
+
+    {
+        Min = 525,
+        Max = 550,
+
+        QuestName = "SkyExp2Quest",
+        QuestLevel = 1,
+
+        Enemy = "Royal Squad",
+
+        QuestPosition = Vector3.new(
+            -7907,
+            5636,
+            -1411
+        )
+    },
+
+    {
+        Min = 550,
+        Max = 625,
+
+        QuestName = "SkyExp2Quest",
+        QuestLevel = 2,
+
+        Enemy = "Royal Soldier",
+
+        QuestPosition = Vector3.new(
+            -7907,
+            5636,
+            -1411
+        )
+    },
+
+    {
+        Min = 625,
+        Max = 650,
+
+        QuestName = "FountainQuest",
+        QuestLevel = 1,
+
+        Enemy = "Galley Pirate",
+
+        QuestPosition = Vector3.new(
+            5255,
+            38,
+            4050
+        )
+    },
+
+    {
+        Min = 650,
+        Max = 700,
+
+        QuestName = "FountainQuest",
+        QuestLevel = 2,
+
+        Enemy = "Galley Captain",
+
+        QuestPosition = Vector3.new(
+            5255,
+            38,
+            4050
+        )
+    }
+
+}
+
