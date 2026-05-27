fdc006ce041e31624e22cc97993dee9c397cdf49
ZenithDev0Br <zenithdev.br@gmail.com>
2026-05-27 01:01:11 -0300

    Create MobScanner.lua

---
+++ Modules/MobScanner.lua
@@ -0,0 +1,16 @@
+-- Modules/MobScanner.lua
+local MobScanner = {}
+
+function MobScanner:GetAll()
+    return {}
+end
+
+function MobScanner:GetClosest(tagName, originPosition)
+    return nil
+end
+
+function MobScanner:GetByName(name)
+    return nil
+end
+
+return MobScanner
