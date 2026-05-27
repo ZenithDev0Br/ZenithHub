0eb43f165008649aedf37ec9c3962799ac1e4c11
ZenithDev0Br <zenithdev.br@gmail.com>
2026-05-27 00:45:08 -0300

    Create Sea3.lua

---
+++ Modules/QuestData/Sea3.lua
@@ -0,0 +1,627 @@
+return {
+
+    {
+        Min = 1500,
+        Max = 1525,
+
+        QuestName = "PortTownQuest",
+        QuestLevel = 1,
+
+        Enemy = "Pirate Millionaire",
+
+        QuestPosition = Vector3.new(
+            -290,
+            7,
+            5328
+        )
+    },
+
+    {
+        Min = 1525,
+        Max = 1575,
+
+        QuestName = "PortTownQuest",
+        QuestLevel = 2,
+
+        Enemy = "Pistol Billionaire",
+
+        QuestPosition = Vector3.new(
+            -290,
+            7,
+            5328
+        )
+    },
+
+    {
+        Min = 1575,
+        Max = 1600,
+
+        QuestName = "HydraIslandQuest",
+        QuestLevel = 1,
+
+        Enemy = "Dragon Crew Warrior",
+
+        QuestPosition = Vector3.new(
+            5704,
+            602,
+            199
+        )
+    },
+
+    {
+        Min = 1600,
+        Max = 1625,
+
+        QuestName = "HydraIslandQuest",
+        QuestLevel = 2,
+
+        Enemy = "Dragon Crew Archer",
+
+        QuestPosition = Vector3.new(
+            5704,
+            602,
+            199
+        )
+    },
+
+    {
+        Min = 1625,
+        Max = 1650,
+
+        QuestName = "GreatTreeQuest",
+        QuestLevel = 1,
+
+        Enemy = "Marine Soldier",
+
+        QuestPosition = Vector3.new(
+            4076,
+            706,
+            -1711
+        )
+    },
+
+    {
+        Min = 1650,
+        Max = 1700,
+
+        QuestName = "GreatTreeQuest",
+        QuestLevel = 2,
+
+        Enemy = "Marine Officer",
+
+        QuestPosition = Vector3.new(
+            4076,
+            706,
+            -1711
+        )
+    },
+
+    {
+        Min = 1700,
+        Max = 1725,
+
+        QuestName = "TurtleQuest1",
+        QuestLevel = 1,
+
+        Enemy = "Fishman Raider",
+
+        QuestPosition = Vector3.new(
+            -2142,
+            43,
+            -10141
+        )
+    },
+
+    {
+        Min = 1725,
+        Max = 1750,
+
+        QuestName = "TurtleQuest1",
+        QuestLevel = 2,
+
+        Enemy = "Fishman Captain",
+
+        QuestPosition = Vector3.new(
+            -2142,
+            43,
+            -10141
+        )
+    },
+
+    {
+        Min = 1750,
+        Max = 1775,
+
+        QuestName = "TurtleQuest2",
+        QuestLevel = 1,
+
+        Enemy = "Forest Pirate",
+
+        QuestPosition = Vector3.new(
+            -1823,
+            43,
+            -9533
+        )
+    },
+
+    {
+        Min = 1775,
+        Max = 1800,
+
+        QuestName = "TurtleQuest2",
+        QuestLevel = 2,
+
+        Enemy = "Mythological Pirate",
+
+        QuestPosition = Vector3.new(
+            -1823,
+            43,
+            -9533
+        )
+    },
+
+    {
+        Min = 1800,
+        Max = 1825,
+
+        QuestName = "MansionQuest",
+        QuestLevel = 1,
+
+        Enemy = "Jungle Pirate",
+
+        QuestPosition = Vector3.new(
+            -12463,
+            331,
+            -5565
+        )
+    },
+
+    {
+        Min = 1825,
+        Max = 1850,
+
+        QuestName = "MansionQuest",
+        QuestLevel = 2,
+
+        Enemy = "Musketeer Pirate",
+
+        QuestPosition = Vector3.new(
+            -12463,
+            331,
+            -5565
+        )
+    },
+
+    {
+        Min = 1850,
+        Max = 1875,
+
+        QuestName = "HauntedQuest1",
+        QuestLevel = 1,
+
+        Enemy = "Reborn Skeleton",
+
+        QuestPosition = Vector3.new(
+            -1554,
+            40,
+            -14290
+        )
+    },
+
+    {
+        Min = 1875,
+        Max = 1900,
+
+        QuestName = "HauntedQuest1",
+        QuestLevel = 2,
+
+        Enemy = "Living Zombie",
+
+        QuestPosition = Vector3.new(
+            -1554,
+            40,
+            -14290
+        )
+    },
+
+    {
+        Min = 1900,
+        Max = 1925,
+
+        QuestName = "HauntedQuest2",
+        QuestLevel = 1,
+
+        Enemy = "Demonic Soul",
+
+        QuestPosition = Vector3.new(
+            -1554,
+            40,
+            -14290
+        )
+    },
+
+    {
+        Min = 1925,
+        Max = 1975,
+
+        QuestName = "HauntedQuest2",
+        QuestLevel = 2,
+
+        Enemy = "Posessed Mummy",
+
+        QuestPosition = Vector3.new(
+            -1554,
+            40,
+            -14290
+        )
+    },
+
+    {
+        Min = 1975,
+        Max = 2000,
+
+        QuestName = "IceCreamQuest1",
+        QuestLevel = 1,
+
+        Enemy = "Peanut Scout",
+
+        QuestPosition = Vector3.new(
+            -1107,
+            43,
+            -11494
+        )
+    },
+
+    {
+        Min = 2000,
+        Max = 2025,
+
+        QuestName = "IceCreamQuest1",
+        QuestLevel = 2,
+
+        Enemy = "Peanut President",
+
+        QuestPosition = Vector3.new(
+            -1107,
+            43,
+            -11494
+        )
+    },
+
+    {
+        Min = 2025,
+        Max = 2050,
+
+        QuestName = "IceCreamQuest2",
+        QuestLevel = 1,
+
+        Enemy = "Ice Cream Squad",
+
+        QuestPosition = Vector3.new(
+            -1150,
+            43,
+            -11550
+        )
+    },
+
+    {
+        Min = 2050,
+        Max = 2075,
+
+        QuestName = "IceCreamQuest2",
+        QuestLevel = 2,
+
+        Enemy = "Ice Cream Commander",
+
+        QuestPosition = Vector3.new(
+            -1150,
+            43,
+            -11550
+        )
+    },
+
+    {
+        Min = 2075,
+        Max = 2100,
+
+        QuestName = "CakeQuest1",
+        QuestLevel = 1,
+
+        Enemy = "Cake Guard",
+
+        QuestPosition = Vector3.new(
+            -1853,
+            43,
+            -12100
+        )
+    },
+
+    {
+        Min = 2100,
+        Max = 2125,
+
+        QuestName = "CakeQuest1",
+        QuestLevel = 2,
+
+        Enemy = "Baking Chef",
+
+        QuestPosition = Vector3.new(
+            -1853,
+            43,
+            -12100
+        )
+    },
+
+    {
+        Min = 2125,
+        Max = 2150,
+
+        QuestName = "CakeQuest2",
+        QuestLevel = 1,
+
+        Enemy = "Head Chef",
+
+        QuestPosition = Vector3.new(
+            -1900,
+            43,
+            -12200
+        )
+    },
+
+    {
+        Min = 2150,
+        Max = 2200,
+
+        QuestName = "CakeQuest2",
+        QuestLevel = 2,
+
+        Enemy = "Cookie Crafter",
+
+        QuestPosition = Vector3.new(
+            -1900,
+            43,
+            -12200
+        )
+    },
+
+    {
+        Min = 2200,
+        Max = 2225,
+
+        QuestName = "ChocQuest1",
+        QuestLevel = 1,
+
+        Enemy = "Cocoa Warrior",
+
+        QuestPosition = Vector3.new(
+            -2300,
+            43,
+            -12000
+        )
+    },
+
+    {
+        Min = 2225,
+        Max = 2250,
+
+        QuestName = "ChocQuest1",
+        QuestLevel = 2,
+
+        Enemy = "Chocolate Bar Battler",
+
+        QuestPosition = Vector3.new(
+            -2300,
+            43,
+            -12000
+        )
+    },
+
+    {
+        Min = 2250,
+        Max = 2275,
+
+        QuestName = "ChocQuest2",
+        QuestLevel = 1,
+
+        Enemy = "Sweet Thief",
+
+        QuestPosition = Vector3.new(
+            -2400,
+            43,
+            -12100
+        )
+    },
+
+    {
+        Min = 2275,
+        Max = 2300,
+
+        QuestName = "ChocQuest2",
+        QuestLevel = 2,
+
+        Enemy = "Candy Rebel",
+
+        QuestPosition = Vector3.new(
+            -2400,
+            43,
+            -12100
+        )
+    },
+
+    {
+        Min = 2300,
+        Max = 2325,
+
+        QuestName = "CandyQuest1",
+        QuestLevel = 1,
+
+        Enemy = "Candy Pirate",
+
+        QuestPosition = Vector3.new(
+            -2600,
+            43,
+            -12300
+        )
+    },
+
+    {
+        Min = 2325,
+        Max = 2350,
+
+        QuestName = "CandyQuest1",
+        QuestLevel = 2,
+
+        Enemy = "Snow Cone Warrior",
+
+        QuestPosition = Vector3.new(
+            -2600,
+            43,
+            -12300
+        )
+    },
+
+    {
+        Min = 2350,
+        Max = 2375,
+
+        QuestName = "TikiQuest1",
+        QuestLevel = 1,
+
+        Enemy = "Sunfire Hunter",
+
+        QuestPosition = Vector3.new(
+            -15400,
+            10,
+            200
+        )
+    },
+
+    {
+        Min = 2375,
+        Max = 2400,
+
+        QuestName = "TikiQuest1",
+        QuestLevel = 2,
+
+        Enemy = "Island Guard",
+
+        QuestPosition = Vector3.new(
+            -15400,
+            10,
+            200
+        )
+    },
+
+    {
+        Min = 2400,
+        Max = 2425,
+
+        QuestName = "TikiQuest2",
+        QuestLevel = 1,
+
+        Enemy = "Isle Scout",
+
+        QuestPosition = Vector3.new(
+            -15500,
+            10,
+            300
+        )
+    },
+
+    {
+        Min = 2425,
+        Max = 2450,
+
+        QuestName = "TikiQuest2",
+        QuestLevel = 2,
+
+        Enemy = "Isle Outlaw",
+
+        QuestPosition = Vector3.new(
+            -15500,
+            10,
+            300
+        )
+    },
+
+    {
+        Min = 2450,
+        Max = 2525,
+
+        QuestName = "TikiQuest3",
+        QuestLevel = 1,
+
+        Enemy = "Island Boy",
+
+        QuestPosition = Vector3.new(
+            -15600,
+            10,
+            400
+        )
+    },
+
+    {
+        Min = 2525,
+        Max = 2600,
+
+        QuestName = "TikiQuest3",
+        QuestLevel = 2,
+
+        Enemy = "Sunfire Captain",
+
+        QuestPosition = Vector3.new(
+            -15600,
+            10,
+            400
+        )
+    },
+
+    {
+        Min = 2600,
+        Max = 2675,
+
+        QuestName = "TikiQuest4",
+        QuestLevel = 1,
+
+        Enemy = "Tiki Outlaw",
+
+        QuestPosition = Vector3.new(
+            -15500,
+            10,
+            300
+        )
+    },
+
+    {
+        Min = 2675,
+        Max = 2750,
+
+        QuestName = "TikiQuest4",
+        QuestLevel = 2,
+
+        Enemy = "Sunfire Elite",
+
+        QuestPosition = Vector3.new(
+            -15600,
+            10,
+            400
+        )
+    },
+
+    {
+        Min = 2750,
+        Max = 2800,
+
+        QuestName = "TikiQuest4",
+        QuestLevel = 2,
+
+        Enemy = "Sunfire Captain",
+
+        QuestPosition = Vector3.new(
+            -15600,
+            10,
+            400
+        )
+    }
+
+}
