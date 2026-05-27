72c9fdf7e10d9a915825291d4438de9c5f20f108
ZenithDev0Br <zenithdev.br@gmail.com>
2026-05-27 00:43:53 -0300

    Create Sea2.lua

---
+++ Modules/QuestData/Sea2.lua
@@ -0,0 +1,355 @@
+return {
+
+    {
+        Min = 700,
+        Max = 725,
+
+        QuestName = "Area1Quest",
+        QuestLevel = 1,
+
+        Enemy = "Raider",
+
+        QuestPosition = Vector3.new(
+            -425,
+            73,
+            1833
+        )
+    },
+
+    {
+        Min = 725,
+        Max = 775,
+
+        QuestName = "Area1Quest",
+        QuestLevel = 2,
+
+        Enemy = "Mercenary",
+
+        QuestPosition = Vector3.new(
+            -425,
+            73,
+            1833
+        )
+    },
+
+    {
+        Min = 775,
+        Max = 800,
+
+        QuestName = "Area2Quest",
+        QuestLevel = 1,
+
+        Enemy = "Swan Pirate",
+
+        QuestPosition = Vector3.new(
+            -1930,
+            38,
+            2038
+        )
+    },
+
+    {
+        Min = 800,
+        Max = 875,
+
+        QuestName = "Area2Quest",
+        QuestLevel = 2,
+
+        Enemy = "Factory Worker",
+
+        QuestPosition = Vector3.new(
+            -1930,
+            38,
+            2038
+        )
+    },
+
+    {
+        Min = 875,
+        Max = 900,
+
+        QuestName = "GreenZoneQuest",
+        QuestLevel = 1,
+
+        Enemy = "Marine Soldier",
+
+        QuestPosition = Vector3.new(
+            -2444,
+            73,
+            -264
+        )
+    },
+
+    {
+        Min = 900,
+        Max = 950,
+
+        QuestName = "GreenZoneQuest",
+        QuestLevel = 2,
+
+        Enemy = "Marine Officer",
+
+        QuestPosition = Vector3.new(
+            -2444,
+            73,
+            -264
+        )
+    },
+
+    {
+        Min = 950,
+        Max = 975,
+
+        QuestName = "GraveyardQuest",
+        QuestLevel = 1,
+
+        Enemy = "Zombie",
+
+        QuestPosition = Vector3.new(
+            -5431,
+            16,
+            -725
+        )
+    },
+
+    {
+        Min = 975,
+        Max = 1000,
+
+        QuestName = "GraveyardQuest",
+        QuestLevel = 2,
+
+        Enemy = "Vampire",
+
+        QuestPosition = Vector3.new(
+            -5431,
+            16,
+            -725
+        )
+    },
+
+    {
+        Min = 1000,
+        Max = 1050,
+
+        QuestName = "SnowMountainQuest",
+        QuestLevel = 1,
+
+        Enemy = "Snow Trooper",
+
+        QuestPosition = Vector3.new(
+            603,
+            401,
+            -5369
+        )
+    },
+
+    {
+        Min = 1050,
+        Max = 1100,
+
+        QuestName = "SnowMountainQuest",
+        QuestLevel = 2,
+
+        Enemy = "Winter Warrior",
+
+        QuestPosition = Vector3.new(
+            603,
+            401,
+            -5369
+        )
+    },
+
+    {
+        Min = 1100,
+        Max = 1125,
+
+        QuestName = "FireSideQuest",
+        QuestLevel = 1,
+
+        Enemy = "Lab Subordinate",
+
+        QuestPosition = Vector3.new(
+            -5235,
+            15,
+            -5023
+        )
+    },
+
+    {
+        Min = 1125,
+        Max = 1175,
+
+        QuestName = "FireSideQuest",
+        QuestLevel = 2,
+
+        Enemy = "Horned Pirate",
+
+        QuestPosition = Vector3.new(
+            -5235,
+            15,
+            -5023
+        )
+    },
+
+    {
+        Min = 1175,
+        Max = 1200,
+
+        QuestName = "IceSideQuest",
+        QuestLevel = 1,
+
+        Enemy = "Magma Ninja",
+
+        QuestPosition = Vector3.new(
+            -6083,
+            16,
+            -4784
+        )
+    },
+
+    {
+        Min = 1200,
+        Max = 1250,
+
+        QuestName = "IceSideQuest",
+        QuestLevel = 2,
+
+        Enemy = "Snow Lurker",
+
+        QuestPosition = Vector3.new(
+            -6083,
+            16,
+            -4784
+        )
+    },
+
+    {
+        Min = 1250,
+        Max = 1275,
+
+        QuestName = "ShipQuest1",
+        QuestLevel = 1,
+
+        Enemy = "Ship Deckhand",
+
+        QuestPosition = Vector3.new(
+            -9514,
+            164,
+            5786
+        )
+    },
+
+    {
+        Min = 1275,
+        Max = 1300,
+
+        QuestName = "ShipQuest1",
+        QuestLevel = 2,
+
+        Enemy = "Ship Engineer",
+
+        QuestPosition = Vector3.new(
+            -9514,
+            164,
+            5786
+        )
+    },
+
+    {
+        Min = 1300,
+        Max = 1325,
+
+        QuestName = "ShipQuest2",
+        QuestLevel = 1,
+
+        Enemy = "Ship Steward",
+
+        QuestPosition = Vector3.new(
+            -9514,
+            164,
+            5786
+        )
+    },
+
+    {
+        Min = 1325,
+        Max = 1350,
+
+        QuestName = "ShipQuest2",
+        QuestLevel = 2,
+
+        Enemy = "Ship Officer",
+
+        QuestPosition = Vector3.new(
+            -9514,
+            164,
+            5786
+        )
+    },
+
+    {
+        Min = 1350,
+        Max = 1400,
+
+        QuestName = "IceCastleQuest",
+        QuestLevel = 1,
+
+        Enemy = "Arctic Warrior",
+
+        QuestPosition = Vector3.new(
+            5650,
+            28,
+            -6484
+        )
+    },
+
+    {
+        Min = 1400,
+        Max = 1425,
+
+        QuestName = "IceCastleQuest",
+        QuestLevel = 2,
+
+        Enemy = "Snow Bandit",
+
+        QuestPosition = Vector3.new(
+            5650,
+            28,
+            -6484
+        )
+    },
+
+    {
+        Min = 1425,
+        Max = 1450,
+
+        QuestName = "ForgottenQuest",
+        QuestLevel = 1,
+
+        Enemy = "Sea Soldier",
+
+        QuestPosition = Vector3.new(
+            -3054,
+            240,
+            -10144
+        )
+    },
+
+    {
+        Min = 1450,
+        Max = 1500,
+
+        QuestName = "ForgottenQuest",
+        QuestLevel = 2,
+
+        Enemy = "Water Artisan",
+
+        QuestPosition = Vector3.new(
+            -3054,
+            240,
+            -10144
+        )
+    }
+
+}
